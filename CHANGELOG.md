120b9907ac3370768fc6c48022f7225cc4c57827
KOREAparksh <tmdgks2222@naver.com>
2022-10-15 21:27:40 +0900

    🎉 init: init first package

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+## 0.0.1
+
+* Notification list view for Android and iOS
---
+++ lib/notification_listview.dart
@@ -0,0 +1,12 @@
+library notification_listview;
+
+import 'package:flutter/material.dart';
+
+class NotificationListView extends StatelessWidget {
+  const NotificationListView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
